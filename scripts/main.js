563311aa64512cd0956b4263847562a9081de1ae
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-14 21:51:21 +0300

    Create: and import script file

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log("imported")
+  const m = document.getElementById("switch-menu")
+  const icon = m.getElementsByTagName("i")[0]
+  const navbar = document.getElementById("desktop-nav")
+  m.addEventListener("click", (e) => {
+
+  })
+})
