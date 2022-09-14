5abd9248091c571121ef5acbd6cdf6f0674f40aa
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-14 21:58:21 +0300

    Update: close Icon switch

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const icon = m.getElementsByTagName("i")[0]
   const navbar = document.getElementById("desktop-nav")
   m.addEventListener("click", (e) => {
-
+    if (icon.classList.contains("fa-bars")) {
+      icon.classList.remove("fa-bars")
+      icon.classList.add("fa-times")
+    }
+    else {
+      icon.classList.add("fa-bars")
+      icon.classList.remove("fa-times")
+    }
   })
 })
