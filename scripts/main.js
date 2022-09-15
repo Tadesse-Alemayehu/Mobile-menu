724fa51d02028b2941f27d164b8641379eda073b
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-15 18:15:48 +0300

    Fix: linter issues

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,19 @@
-
-document.addEventListener("DOMContentLoaded", (e) => {
-  console.log("imported")
-  const m = document.getElementById("switch-menu")
-  const icon = m.getElementsByTagName("i")[0]
-  const navbar = document.getElementById("desktop-nav")
-  m.addEventListener("click", (e) => {
-    if (icon.classList.contains("fa-bars")) {
-      icon.classList.remove("fa-bars")
-      icon.classList.add("fa-times")
-      navbar.classList.add("mobile-shown")
-      navbar.style.display = 'none'
+document.addEventListener('DOMContentLoaded', (e) => {
+  console.log('imported');
+  const m = document.getElementById('switch-menu');
+  const icon = m.getElementsByTagName('i')[0];
+  const navbar = document.getElementById('desktop-nav');
+  m.addEventListener('click', () => {
+    if (icon.classList.contains('fa-bars')) {
+      icon.classList.remove('fa-bars');
+      icon.classList.add('fa-times');
+      navbar.classList.add('mobile-shown');
+      navbar.style.display = 'none';
+    } else {
+      icon.classList.add('fa-bars');
+      icon.classList.remove('fa-times');
+      navbar.classList.remove('mobile');
+      navbar.style.display = 'flex';
     }
-    else {
-      icon.classList.add("fa-bars")
-      icon.classList.remove("fa-times")
-      navbar.classList.remove("mobile")
-      navbar.style.display = 'flex'
-    }
-  })
-
-  if (navbar.style.valueOf()['display'] == 'none') {
-    console.log("navbar is hidden")
-  }
-})
+  });
+});
