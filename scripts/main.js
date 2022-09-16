425300f11fd47f9a806a9fff1582eac70f7a3fba
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-16 03:47:15 +0300

    Improve: pop-up height and widths

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('imported');
-  const m = document.getElementById('switch-menu');
-  const icon = m.getElementsByTagName('i')[0];
+  const menu = document.getElementById('switch-menu');
+  const icon = menu.getElementsByTagName('i')[0];
   const navbar = document.getElementById('desktop-nav');
-  m.addEventListener('click', () => {
+  const logo = document.getElementById('logo');
+  const header = document.getElementsByTagName('header')[0];
+  menu.addEventListener('click', () => {
     if (icon.classList.contains('fa-bars')) {
       icon.classList.remove('fa-bars');
       icon.classList.add('fa-times');
       navbar.classList.add('mobile-shown');
       navbar.style.display = 'none';
+      logo.style.display = 'none';
+      header.style.minHeight = '100vh';
     } else {
       icon.classList.add('fa-bars');
       icon.classList.remove('fa-times');
       navbar.classList.remove('mobile');
       navbar.style.display = 'flex';
+      logo.style.display = 'flex';
+      header.style.minHeight = 'max-content';
     }
   });
 });
